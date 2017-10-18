e7a381e407efdb5a3055d36d3bee1f006034928d
Sargis <Sargis@WINDOWS-K9TM8ME>
2017-10-18 22:50:54 +0400

    First commit

---
+++ src/inventorymanagment/InventoryManagment.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inventorymanagment;
+
+import inventorymanagment.objectTemplates.ConnectionFactory;
+import inventorymanagment.objectTemplates.Goods;
+import inventorymanagment.objectTemplates.GoodsInput;
+import inventorymanagment.objectTemplates.Sale;
+import java.text.ParseException;
+
+
+
+/**
+ *
+ * @author Sargis
+ */
+public class InventoryManagment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws ParseException {
+        // TODO code application logic here
+        Goods good = new Goods("B6");
+        GoodsInput gi = new GoodsInput(good,3,5,2.5);
+        //ConnectionFactory.inputGoods(gi);
+        Sale sale = new Sale("20/10/17", "A3", 3, 2.2);
+        ConnectionFactory.inputSales(sale);
+        
+    
+        
+    }
+    
+}
